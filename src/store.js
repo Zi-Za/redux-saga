079cce33f2a132c21de8c76dc5bf5944aa7a9b7a
Zina Zadorozhnaya <Zadorozhnaya@mail.ru>
2023-06-26 12:17:55 +0700

    fix saga problem

--- src/store.js
+++ src/store.js
@@ -1,14 +1,16 @@
-import { createStore } from "redux"
-import { applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 import createSagaMiddleware from 'redux-saga';
 import { watchFetchDog } from './sagas';
 
 import { dogReducer } from './reducer';
 
 const sagaMiddleware = createSagaMiddleware();
-export const store = createStore(
-    dogReducer, 
-    applyMiddleware(sagaMiddleware)
-);
+
+export const store = configureStore({
+    reducer: dogReducer,
+    middleware: [sagaMiddleware],
+    devTools: true
+})
+
 sagaMiddleware.run(watchFetchDog);
 
